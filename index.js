95ca4bcd9d1f7435432669fc596973b0f0bc0d8f
Alexei Vacari <47106150+Alexei001@users.noreply.github.com>
2019-11-10 13:08:51 +0200

    add slide

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Slider code
+let sliderIndex = 0 ;
+
+showSlider(sliderIndex);
+
+function next(){
+    showSlider(++sliderIndex);
+}
+
+
+function prev(){
+    showSlider(--sliderIndex);
+}
+
+function currentSlide(index){
+    showSlider(index);
+}
+
+
+function showSlider(index){
+    let slides = document.getElementsByClassName('mySlides');
+    let dots = document.getElementsByClassName('dot');
+    
+    sliderIndex = index;
+    if (index <0){sliderIndex = slides.length -1 }
+    if (index >= slides.length){sliderIndex = 0 }
+
+    //if(index > slides.length)
+
+    for(let i = 0 ; i < slides.length ; i++){
+        slides[i].style.display = 'none';
+        dots[i].classList.remove('active');
+    }
+    slides[sliderIndex].style.display = 'block';
+    dots[sliderIndex].classList.add('active');
+}
